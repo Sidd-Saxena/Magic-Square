173480fc7568855a6100ad886fcd2374f4635c03
Sidd-Saxena <saxena.siddharth1992@gmail.com>
2014-10-13 16:05:59 -0400

    Final commit

--- Magicsquare2.c
+++ Magicsquare2.c
@@ -8,34 +8,38 @@ void initSquare( MagSquare_PTR x, int magicsquaresize )
 {
 	srand(time(NULL));//seed the random number generator 
 	int m,i,j;
-	//int i,l;
     //initilizing the array for random number generation
 	int a[magicsquaresize*magicsquaresize];
+	//loop for initializing an array for creating random nos
 	for (m=0; m<(magicsquaresize*magicsquaresize); m++)
 	{
 		a[m]=0;
 	}
-	/*Random(magicsquaresize,a);
-  		  for (l=0;l<(magicsquaresize*magicsquaresize);l++)
-  			{
-  	  			printf("%d\n",a[l]);
-  	   		}*/
-	//int n=magicsquaresize*magicsquaresize;
-	//int y[n]=random(n)
+	*x[0][0]=16;
+	*x[0][1]=3;
+	*x[0][2]=2;
+    *x[0][3]=13;
+	*x[1][0]=5;
+	*x[1][1]=10;
+	*x[1][2]=11;
+    *x[1][3]=8;
+	*x[2][0]=9;
+	*x[2][1]=6;
+	*x[2][2]=7;
+    *x[2][3]=4;
+	*x[3][0]=12;
+	*x[3][1]=15;
+	*x[3][2]=14;
+	*x[3][3]=1;
+	/*//for loop to initialize the matrix with random nos
      for(i=0; i < magicsquaresize; i++)
       {
-      	 //srand(time(NULL));//seed the random number generator 
          for(j=0; j < magicsquaresize; j++) 
   		  {
-
-  		  		//*x[i][j] =1 + rand( ) % (magicsquaresize*magicsquaresize);  
-  		  		/*l=Random(magicsquaresize,a);
-  		  		printf("%d\n",l);*/
-
-  		  		*x[i][j]=Random(magicsquaresize,a);	
-  		  		//printf("%d\n",*x[i][j] );		  				   	
+  		  	//calling the random function to enter into the matrix
+  		  		*x[i][j]=Random(magicsquaresize,a);		  				   	
   		  }
-	  }
+	  }*/
 }
 
 //function to print the magic square matrix
@@ -78,6 +82,7 @@ int sumRow(int row, MagSquare_PTR x, int magicsquaresize)
 int sumDiagonal( int diagonal, MagSquare_PTR x, int magicsquaresize )
 {
 	int i,j,sum=0;
+	//diagonal for top left to bottom right
 	if(diagonal==0)
 	{
 		for (i = 0; i < magicsquaresize; i++)
@@ -85,7 +90,7 @@ int sumDiagonal( int diagonal, MagSquare_PTR x, int magicsquaresize )
 			sum = sum + *x[i][i];
 		}
 	}
-
+//diagonal for top right to bottom left
 	if(diagonal==1)
 	{
 		i=0;
@@ -95,47 +100,62 @@ int sumDiagonal( int diagonal, MagSquare_PTR x, int magicsquaresize )
 			sum = sum + *x[i][j];
 			i=i+1;
 			j=j-1;
-          }
-				
-			
+          }		
 	}
 	return sum;
 }
 
 // function to check if the square is magic or not
+// function to check if the square is magic or not
 int isMagic(MagSquare_PTR x, int magicsquaresize )
 {
-	int n = (magicsquaresize * ((magicsquaresize*magicsquaresize)+1));
-	int i,flag = 0;
+	int n = ((magicsquaresize * ((magicsquaresize*magicsquaresize)+1)))/2;
+	//printf("%d\n",n );
+	int i=0;
+	int flag_cols,flag_row,flag_diag = 0;
 	for (i = 0 ;i < magicsquaresize ; i++)
 	{
 		if (n == sumColumn( i, x, magicsquaresize ))
 		{
-			flag =1;
+			//printf("Coloum passed\n");
+			flag_cols = 1;
+		} 
+		if (n != sumColumn( i, x, magicsquaresize ))
+		{
+			flag_cols = 0;
+			//printf("Coloumn failed\n");
+			break;
 		}
 	}
-	while (flag == 1)
-	{
-		for (i = 0 ;i < magicsquaresize ; i++)
+	for (i = 0 ;i < magicsquaresize ; i++)
 	{
 		if (n == sumRow( i, x, magicsquaresize))
 		{
-			flag = 1;
+			flag_row = 1;
+			//printf("row passed\n");
 		}
-
-	}
-	}
-    while (flag == 1)
-    {
-		for (i= 0 ;i < 2; i++)
+		if (n != sumRow( i, x, magicsquaresize ))
 		{
-			if (n == sumDiagonal( i, x, magicsquaresize))
-			{
-				flag = 1;
-			}
+			flag_row=0;
+			//printf("Row failed\n");
+			break;
 		}
 	}
- 	if (flag == 1)
+	for (i= 0 ;i < 2; i++)
+	{
+		if (n == sumDiagonal( i, x, magicsquaresize))
+		{
+			flag_diag = 1;
+			//printf("Diagonal passed\n");
+		}
+		if (n != sumDiagonal( i, x, magicsquaresize ))
+		{
+				flag_diag=0;
+				//printf("Diagonal failed\n");
+				break;
+		}
+	}
+ 	if (flag_diag == 1 && flag_row == 1 && flag_cols == 1)
  	{
  		return 1;
  	}
@@ -144,17 +164,25 @@ int isMagic(MagSquare_PTR x, int magicsquaresize )
  		return 0;
  	}
 }
-//function to permutate in order to get Magic square
 
-void permuteSquare(MagSquare_PTR x)
+//function to permutate in order to get Magic square
+void permuteSquare(MagSquare_PTR x, int magicsquaresize )
 {
-	srand(time(NULL));//seed the random number generator 
-	
+	//srand(time(NULL));//seed the random number generator 
+	//time_t t;
+	//srand((unsigned)time(&t));
+	int n =magicsquaresize;
 	int i,j,k,m,temp;
-	i = rand() % 2;
-	j = rand() % 2;
-	k = rand() % 2;
-	m = rand() % 2;
+	do{
+	i = rand() % n;
+	j = rand() % n;
+	k = rand() % n;
+	m = rand() % n;
+	}while(i==k && j==m);
+ /*printf("%d  ",i);
+ printf("%d  ",j);
+ printf("%d  ",k);
+ printf("%d\n",m);*/
 
 	temp = *x[i][j];
 	*x[i][j] = *x[k][m];
@@ -163,21 +191,18 @@ void permuteSquare(MagSquare_PTR x)
 // to generate random function
 int Random (int magicsquaresize,int a[])
 {
-	//srand(time(NULL));//seed the random number generator 
 	int n= rand()%(magicsquaresize*magicsquaresize);
-    if (a[n]==0)
+	//check wheather the number is repeated or not
+    if (a[n]==0)//not repeated
     {
-	  a[n]=-1;
-	  return(n+1);
-	  //printf("%d\n",n );
-	  //printf("Different\n" );
+	  a[n]=-1;//change the flag
+	  return(n+1);//return the value
     }
-    else if(a[n]==-1)
+    else if(a[n]==-1)//repeated
     {
+    	//call the function again
     	Random (magicsquaresize,a);
-    	//printf("calling function again\n");
     }
-    //printf("exiting loop" );
-    //printf("%d\n",n+1);
-   // return (n+1);
-}
+    //return(n+1);//return the vaue
+ }
+
--- Maintest2.c
+++ Maintest2.c
@@ -6,34 +6,88 @@
 
 int main()
 {
+	time_t start,end;
     //seeding the random number generator
 	srand(time(NULL)); 
-	//int count=0;
+
+	//initializing the counter to count the number of iterations
+	int count=0;
+
 	//dynamic allocation of memory at pointer location using malloc
 	MagSquare_PTR mptr = (MagSquare_PTR)malloc(sizeof(MSQUARE_TYPE));
+
+
 	//calling function to initialize the array
 	initSquare( mptr, MAGICSIZE );
+
 	//calling function to print the array
 	printSquare( mptr , MAGICSIZE);
-	//calling function to check weather the 2-D array is magic or not
 /*
-	int a = isMagic(mptr, MAGICSIZE );
-	do{		
-	if(a==0)
+	//calling function to calculate the sum of coloumns
+     int i;
+     for (i = 0 ;i < MAGICSIZE ; i++)
+     {
+          printf("The sum of Coloumn %d",(i+1));
+          printf(" is ");
+          printf("%d\n", sumColumn( i, mptr, MAGICSIZE ));
+     }
+
+     //calling function to calculate the sum of coloumns
+     int j;
+     for (j = 0 ;j < MAGICSIZE ; j++)
+     {
+          printf("The sum of Row %d",(j+1));
+          printf(" is ");
+          printf("%d\n", sumRow( j, mptr, MAGICSIZE ));
+     }
+
+     // calling function to calculate the sum of diagonal elememts
+     int k;
+     for (k = 0 ;k < 2; k++)
+     {
+          printf("The sum of diagonal %d",(k+1));
+          printf(" is ");
+          printf("%d\n", sumDiagonal( k, mptr, MAGICSIZE));
+     }
+     */
+     
+	//calling function to check weather the 2-D array is magic or not
+	//int a = isMagic(mptr, MAGICSIZE );
+	int a;
+
+    time(&start);
+	do{	
+		//printf("Checking\n");
+		a=isMagic(mptr, MAGICSIZE );//check if it's a magic square
+		//printf("Entering conditions\n");
+	if(a==0)//not a mgic square hence permute 
 	{
-		count++;
-		permuteSquare(mptr);
+		//printf("Entered Permute square\n");
+		count=count+1;//increment the counter
+		//calling the function to swap two locations
+		permuteSquare(mptr , MAGICSIZE );
+		//printf("Out of permute loop\n");
 		//printSquare(mptr , MAGICSIZE);
-		a=isMagic(mptr, MAGICSIZE );
+		//printf("---------------------------------------------------------\n" );
+		//check again if the matrix is a magic square
+		//a=isMagic(mptr, MAGICSIZE );
+		//printf("%lf\n",count);
+		//printf("Not a magic square\n");
 	}
-	else
+	else if(a==1)//it is a magic square hence print
 	{
+		printf("This is the final Magic Square\n");
 		printSquare( mptr , MAGICSIZE);
 	}
     }while(a!=1);
+    time(&end);
+    double duration;
+	//using the function difftime to calculate the time difference between start and end time
+	duration = difftime(end,start);
+	printf("%f It took following time to get a magic square\n",duration );
     //calling function to permute
     //permuteSquare(MagSquare_PTR x);
-    printf("%d\n",count);
-    */
+    printf("It took following iterations to find the magic square  %d\n",count);
+    
 	return (0);
 }
